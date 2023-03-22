6b573425056ab2a8d0e39e0b65e04930cbd30b8a
Eilish <eilish.summer@gmail.com>
2023-03-22 14:46:54 +1100

    bash script to initialise simulations on different threads

--- scripts/zieria_simulations.sh
+++ scripts/zieria_simulations.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# this script initiates the zieria simulations r script  given different outcross and pop sizes on different threads
+
 input_values=(1 0.25 0.5 0.75 0) #outcross rates
 other_values=(5 10) # pop sizes
 num_cores=25 # set the number of cores to use here
