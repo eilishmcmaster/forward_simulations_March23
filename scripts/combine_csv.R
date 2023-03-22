5b393a5b9aecd23fed091b258b2e754ef39b5039
Eilish <eilish.summer@gmail.com>
2023-03-22 14:41:48 +1100

    this script combines output csvs from the simulations

--- scripts/combine_csv.R
+++ scripts/combine_csv.R
@@ -1,3 +1,9 @@
+
+
+# This script takes the csv outputs from the simulations and combines them. The csvs are the average values per generation for that scenario.
+# This script returns a Ho and a He CSV which can then be used to plot average heterozygosity over n generations.  
+
+
 # set the directory where your csv files are stored
 setwd("/home/eilish/projects/forward_simulations_March23/outputs/zieria_pop_style/dataframes")
 
